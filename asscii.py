836c839b6bc641866575032af72dcee5f1eb2bf3
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2025-12-30 15:34:58 +0530

    Added video feed ascii generation

--- asscii.py
+++ asscii.py
@@ -1,34 +1,41 @@
+import os
+import time
 import cv2
 import numpy as np
 
-# Ñ@#W$9876543210?!abc;:+=-,._
 # table = "Ñ@#W$9876543210?!abc;:+=-,._"
 table = "@%#*+=-:,. "
-
-# image_path = "img.jpg"
-image_path = input("Enter image path: ").strip('"')
-
-img = cv2.imread(image_path)
-if img is None:
-    print("Error: Image not found")
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Couldn't open camera")
     exit()
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    os.system('cls')
+    ret, frame = cap.read()
+    if not ret:
+        print("can't receive frame, exiting....")
+        break
 
-# Resize to a reasonable width (e.g. 100 chars) that fits on a screen
-width = int(input("image size? "))
-height = int(img.shape[0] * (width / img.shape[1]) * 0.55)
-img_gray = cv2.resize(img_gray, (width, height))
+    cv2.imshow("Live Cam Feed", frame)
+    
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    width = 100
+    height = int(frame.shape[0] * (width / frame.shape[1]) * 0.5)
+    frame_gray = cv2.resize(frame_gray, (width, height))
+    
+    img_ascii = ""
+    for i in range(frame_gray.shape[0]):
+        for j in range(frame_gray.shape[1]):
+            img_ascii += table[int(frame_gray[i][j] / 255 * (len(table)-1))]
+        img_ascii += "\n"
+    
+    print(img_ascii)
+    
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
 
-img_ascii = ""
-for i in range(img_gray.shape[0]):
-    for j in range(img_gray.shape[1]):
-        img_ascii += table[int(img_gray[i][j] / 255 * (len(table)-1))]
-    img_ascii += "\n"
-
-with open("img_ascii.txt", "w") as f:
-    f.write(img_ascii)
-
-# cv2.imshow("image_gray", img_gray)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
